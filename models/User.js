97afaf27107993c7b78951cbcf799aa23d1d5031
ahteshamsalamatansari <rdxahteshamsalamat@outlook.com>
2026-06-10 11:45:00 +0530

    "Add JWT signature helper to User model"

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -29,7 +30,6 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-// Encrypt password using bcrypt before save
 UserSchema.pre('save', async function(next) {
   if (!this.isModified('password')) {
     next();
@@ -37,5 +37,17 @@ UserSchema.pre('save', async function(next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function() {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET || 'secret', {
+    expiresIn: process.env.JWT_EXPIRE || '30d'
+  });
+};
+
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
