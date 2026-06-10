517b4ca541e9869f62a9bb0a989aade56abd6842
ahteshamsalamatansari <rdxahteshamsalamat@outlook.com>
2026-06-10 13:15:00 +0530

    "Implement createPost function in postController"

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,25 @@
 const Post = require('../models/Post');
 
-// @desc    Get all posts
-// @route   GET /api/posts
-// @access  Public
 exports.getPosts = async (req, res, next) => {
-  res.status(200).json({ success: true, data: 'Get posts stub' });
+  try {
+    const posts = await Post.find().populate('user', 'username email');
+    res.status(200).json({ success: true, count: posts.length, data: posts });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+
+// @desc    Create new post
+// @route   POST /api/posts
+// @access  Private
+exports.createPost = async (req, res, next) => {
+  try {
+    // Add user ID to request body
+    req.body.user = req.user.id;
+
+    const post = await Post.create(req.body);
+    res.status(201).json({ success: true, data: post });
+  } catch (err) {
+    res.status(400).json({ success: false, error: err.message });
+  }
 };
